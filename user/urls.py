7e9b590ebd7db438762b7fb647d03c3cb3751696
Folafolu <ofolafolu@gmail.com>
2024-07-07 23:02:12 +0100

    url

--- user/urls.py
+++ user/urls.py
@@ -3,13 +3,10 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from .views import AddUserToOrganisationView, LoginView, OrganizationDetailView, OrganizationListview, RegisterView, UserDetailView
 
 urlpatterns = [
-    path("auth/register/", RegisterView.as_view(), name="register"),
+    path("auth/register", RegisterView.as_view(), name="register"),
     path("auth/login/", LoginView.as_view(),name="login"),
-    path('api/organisations/', OrganizationListview.as_view(), name='organisation_list'),
-    path("api/organisations/<uuid:org_id>/", OrganizationDetailView.as_view(), name="organisation_detail"),
+    path('api/organisations', OrganizationListview.as_view(), name='organisation_list'),
+    path("api/organisations/<uuid:org_id>", OrganizationDetailView.as_view(), name="organisation_detail"),
     path('api/organisations/<uuid:org_id>/users', AddUserToOrganisationView.as_view(), name='add_user_to_organisation'),
-    path('api/users/<int:pk>/', UserDetailView.as_view(), name="user_detail"),
-
-
-
+    path('api/users/<int:pk>', UserDetailView.as_view(), name="user_detail"),
 ]
